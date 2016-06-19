6d5e2bb2d9449d68a9e3f6cd18b6dae24e04417c
antoine <tou-toine@hotmail.fr>
2016-06-19 20:21:07 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// scroll
+$(function() {
+    $('body').on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// contact form label
+$(function() {
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+});
+
+// scroll top
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// responsive menu (haut)
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
